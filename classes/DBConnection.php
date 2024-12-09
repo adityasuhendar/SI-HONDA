574bbb592bb08e34d8bbd14c17b1fb1890838f1e
adityasuhendar <suhendaradityawahyu@github.com>
2024-12-09 21:05:55 +0700

    first commit

--- classes/DBConnection.php
+++ classes/DBConnection.php
@@ -1,30 +1,32 @@
 <?php
-if(!defined('DB_SERVER')){
+if(!defined('DB_SERVER')) {
     require_once("../initialize.php");
 }
-class DBConnection{
 
-    private $host = DB_SERVER;
-    private $username = DB_USERNAME;
-    private $password = DB_PASSWORD;
-    private $database = DB_NAME;
+class DBConnection {
+
+    private $host = 'autorack.proxy.rlwy.net';  // Host from the URL
+    private $username = 'root';                  // Username from the URL
+    private $password = 'zIcVZvexmpATLkKKDLrPnnihLByJfijI'; // Password from the URL
+    private $database = 'railway';               // Database name from the URL
+    private $port = 21299;                       // Port from the URL
     
     public $conn;
     
-    public function __construct(){
-
+    public function __construct() {
+        // Try to create a connection
         if (!isset($this->conn)) {
+            $this->conn = new mysqli($this->host, $this->username, $this->password, $this->database, $this->port);
             
-            $this->conn = new mysqli($this->host, $this->username, $this->password, $this->database);
-            
-            if (!$this->conn) {
-                echo 'Cannot connect to database server';
+            if ($this->conn->connect_error) {
+                echo 'Connection failed: ' . $this->conn->connect_error;
                 exit;
-            }            
-        }    
-        
+            }
+        }
     }
-    public function __destruct(){
+
+    public function __destruct() {
+        // Close the connection when done
         $this->conn->close();
     }
 }
