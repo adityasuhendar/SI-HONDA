04b62512bcd14916f015e9628de194641e02cd82
adityasuhendar <suhendaradityawahyu@github.com>
2024-12-18 01:54:52 +0700

    config

--- config.php
+++ config.php
@@ -1,12 +1,14 @@
 <?php
-ob_start();
 ini_set('date.timezone','Asia/Manila');
 date_default_timezone_set('Asia/Manila');
-session_start();
+
+session_start(); // Panggil paling awal
+ob_start();
 
 require_once('initialize.php');
 require_once('classes/DBConnection.php');
 require_once('classes/SystemSettings.php');
+
 $db = new DBConnection;
 $conn = $db->conn;
 
@@ -14,10 +16,10 @@ function redirect($url=''){
 	if(!empty($url))
 	echo '<script>location.href="'.base_url .$url.'"</script>';
 }
+
 function validate_image($file){
     $file = explode("?",$file)[0];
 	if(!empty($file)){
-			// exit;
 		if(is_file(base_app.$file)){
 			return base_url.$file;
 		}else{
@@ -27,6 +29,7 @@ function validate_image($file){
 		return base_url.'dist/img/no-image-available.png';
 	}
 }
+
 function isMobileDevice(){
     $aMobileUA = array(
         '/iphone/i' => 'iPhone', 
@@ -37,14 +40,11 @@ function isMobileDevice(){
         '/webos/i' => 'Mobile'
     );
 
-    //Return true if Mobile User Agent is detected
     foreach($aMobileUA as $sMobileKey => $sMobileOS){
         if(preg_match($sMobileKey, $_SERVER['HTTP_USER_AGENT'])){
             return true;
         }
     }
-    //Otherwise return false..  
     return false;
 }
-ob_end_flush();
 ?>
